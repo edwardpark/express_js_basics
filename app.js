54e4ef5c6510701414586f7d6a43e992cb0f23c3
Edward Park <parkes.edward@gmail.com>
2015-08-16 21:52:32 -0400

    added .write and .end

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-  response.send("hello world");
-
+  response.write("hello world");
+  response.end();
 });
 
-app.listen(3000);
+app.listen(3000, function(){
+  console.log("listening on port 3000 of awesome!");
+});
