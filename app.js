4f5928343eacb12eb2f66a72bc81d87a5cfcbd2e
Edward Park <parkes.edward@gmail.com>
2015-08-16 21:55:36 -0400

    added .send to json

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get('/', function(request, response){
   response.end();
 });
 
+app.get('/blocks',function(request,response){
+  var blocks = ['fixed','moveable','rotating'];
+  response.send(blocks); //send function will convert to json
+});
+
 app.listen(3000, function(){
   console.log("listening on port 3000 of awesome!");
 });
